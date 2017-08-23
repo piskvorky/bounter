78d34600cf1343b070e8361fc53c3147b06a3afa
Filip.Stefanak <filip.stefanak@gmail.com>
2017-08-23 18:32:42 +0200

    Count-min Sketch prototype update: use numpy array - already at peak memory efficiency! test with profiling

--- bounded_counter/count_min_sketch.py
+++ bounded_counter/count_min_sketch.py
@@ -1,5 +1,5 @@
-from collections import Counter
 import os
+import numpy
 
 
 def hash_key(i, key):
@@ -11,73 +11,83 @@ algorithm_logcounter = 2
 
 class CountMinSketch(object):
     def __init__(self, width=16, depth=8, algorithm = None):
+        self.logshift = 0
+        array_type = numpy.uint32
+        self.algorithm = algorithm_basic
+        self.inc_method = self.basic_increment
+        if algorithm and str(algorithm).lower() == 'conservative':
+            self.algorithm = algorithm_conservative
+            self.inc_method = self.conservative_increment
+        if algorithm and str(algorithm).lower() == 'logcounter8':
+            array_type = numpy.uint8
+            self.algorithm = algorithm_logcounter
+            self.logshift = 2
+            self.inc_method = self.log_increment
+        if algorithm and str(algorithm).lower() == 'logcounter1024':
+            array_type = numpy.uint16
+            self.algorithm = algorithm_logcounter
+            self.logshift = 9
+            self.inc_method = self.log_increment
+
         self.width = width
         self.depth = depth
         self.hash_length = width.bit_length()
         self.hash_mask = (1 << (self.hash_length - 1)) - 1
-        self.rows = dict()
-        self.algorithm = algorithm_basic
-        self.logshift = 0
-        if algorithm and str(algorithm).lower() == 'conservative':
-            self.algorithm = algorithm_conservative
-        if algorithm and str(algorithm).lower() == 'logcounter8':
-            self.algorithm = algorithm_logcounter
-            self.logshift = 2
-        if algorithm and str(algorithm).lower() == 'logcounter1024':
-            self.algorithm = algorithm_logcounter
-            self.logshift = 9
         self.logbase = 1 << (self.logshift + 1)
-        for i in range(self.depth):
-            self.rows[i] = Counter()
+        self.table = numpy.zeros((depth, width), array_type)
+
 
     def bucket(self, i, key):
         return self.hash_mask & hash_key(i, key)
 
     def increment(self, key):
-        if self.algorithm == algorithm_basic:
-            self.basic_increment(key)
-        elif self.algorithm == algorithm_conservative:
-            self.conservative_increment(key)
-        elif self.algorithm == algorithm_logcounter:
-            self.log_increment(key)
-        else:
-            raise AssertionError
+        self.inc_method(key)
 
     def basic_increment(self, key):
         for i in range(self.depth):
-            self.rows[i][self.bucket(i, key)] += 1
+            self.table[i][self.bucket(i, key)] += 1
 
     def conservative_increment(self, key):
         current = self[key]
         for i in range(self.depth):
             bucket = self.bucket(i, key)
-            if self.rows[i][bucket] == current:
-                self.rows[i][bucket] += 1
+            if self.table[i, bucket] == current:
+                self.table[i, bucket] += 1
 
     def log_increment(self, key):
         for i in range(self.depth):
             bucket = self.bucket(i, key)
-            current = self.rows[i][bucket]
+            current = int(self.table[i, bucket])
             if current < 2 * self.logbase:
-                self.rows[i][bucket] += 1
+                self.table[i, bucket] += 1
             else:
-                mask = (1 << (int(current/self.logbase) - 1)) - 1
+                mask = (1 << (current//self.logbase - 1)) - 1
                 r = int.from_bytes(os.urandom(4), byteorder='big', signed=False)
                 if (r & mask) == 0:
                     current += 1
-                    self.rows[i][bucket] = current
+                    self.table[i, bucket] = current
 
     def log_decode(self, log_value):
         if (log_value <= 2 * self.logbase):
             return log_value
         else:
-            base = 1 << (int(log_value / self.logbase) + self.logshift)
+            base = 1 << ((log_value >> (self.logshift + 1)) + self.logshift)
             return int(base + (log_value % self.logbase) * (base / self.logbase))
+
+    def log_encode(self, input):
+        value = int(input)
+        if (value <= 2 * self.logbase):
+            return value
+        base_bits = value.bit_length() - 1
+        log_value_base = (base_bits - self.logshift) * self.logbase
+
+        remainder = (value >> (base_bits - self.logshift - 1)) - self.logbase
+        return log_value_base + remainder
 
     def __getitem__(self, key):
         minimum = None
         for i in range(self.depth):
-            current = self.rows[i][self.bucket(i, key)]
+            current = self.table[i, self.bucket(i, key)]
             if (minimum == None):
                 minimum = current
             else:
