b8d057cb5173ca9dfd0245289bc1f444cf6b5c68
Filip.Stefanak <filip.stefanak@gmail.com>
2017-08-30 17:13:51 +0200

    Hyperloglog counter & conservative increment

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# bounded_counter
+Bounded Counter – Count-min Sketch frequency estimation for large sets
+======================================================================
+
+Library implementing [Count-min Sketch](https://en.wikipedia.org/wiki/Count%E2%80%93min_sketch)
+data structure used to estimate frequencies of elements in massive data sets with fixed memory footprint.
+
+Contains implementation of the basic and conservative-update Count-min Sketch table, as well as our own implementation
+using logarithmic probabilistic counter for reducing the space requirement (*logcons1024*).
+
+A [hyperloglog](https://github.com/ascv/HyperLogLog) counter is used to estimate the cardinality of the set of elements (how many distinct elements are tracked).
+
+Parameters
+----------
+
+-   **Width**: The number of columns (hash buckets) in a single row of the table. Must be a power of 2.
+    Significantly affects precision and memory footprint. For precise results, this should be no smaller than one
+    order of magnitude away from the cardinality of the set.
+    For significantly smaller widths, deterioration will occur.
+    For instance, to store all bigrams in English Wikipedia (1 857 420 106 bigrams, 179 413 989 unique),
+    good results can be achieved with a width of 67 108 864 (2^26) (37%).
+-   **Depth**: Number of rows, significant for the reliability of the result. Linearly affects speed of the
+    algorithm as well as its memory footprint. For best results, use a small number such as 5.
+    Using more than 8 is wasteful, better put that memory
+    in width instead or use 'basic' or 'conservative' algorithms instead.
+-   **Algorithm**: There are several algorithms available:
+    -   *basic* straightforward implementation of the Count-Min Sketch as described on
+        [Wikipedia](https://en.wikipedia.org/wiki/Count%E2%80%93min_sketch) with a cell size of 4 bytes.
+    -   *conservative* variant of the algorithm using a simple optimization which is also described on Wikipedia. Also
+        uses 4 bytes per cell.
+    -   *logcounter8* is a probabilistic counter with a value that fits into 1 byte. Values of this counter are usually
+        off by +/- 30%. This can be used for rough estimations for extremely large sets.
+    -   *logcounter1024* is a probabilistic counter with a value that fits into 2 bytes. Values of this counter are usually
+        off by +/- 2%.
+    -   *logcons1024* is a conservative-update upgrade of 'logcounter1024' which offers substantially better precision of
+        the structure.
+
+Memory
+------
+To calculate memory footprint:
+    ( width * depth * cell_size ) + HLL size
+
+Cell size is
+   - 4B for basic / conservative algorithm
+   - 1B for logcounter8
+   - 2B for logcounter1024 / logcons1024
+HLL size is 32 KB
+
+Example:
+    width 2^26 (67 108 864), depth 4, logcons1024 (2B) has 2^(26 + 2 + 1) + 32 768 = 536.9 MB
+    Can be pickled to disk with this exact size.
--- bounded_counter/count_min_sketch.py
+++ bounded_counter/count_min_sketch.py
@@ -2,6 +2,7 @@ import random
 
 import numpy
 import xxhash
+from HLL import HyperLogLog
 
 
 def hash_key(row, key):
@@ -12,43 +13,25 @@ def hash_key(row, key):
     """
     return xxhash.xxh64(str(key), seed=row).intdigest()
 
-"""
-Internal constants
-"""
-algorithm_basic = 0
-algorithm_conservative = 1
-algorithm_logcounter = 2
 
-
-"""
-        Initializes the Count-Min Sketch structure with the given parameters
-        :param width (int) controls the number of hash buckets in one row of the table, should always be power of 2.
-            The more, the better, should be very large, preferably in the same order of magnitude as the cardinality
-            of the counted set.
-        :param depth (int) controls the number of rows of the table. Having more rows decreases probability of a big
-            overestimation. Choose a small number such as 4-8.
-        :param algorithm (string) controls the algorithm used for counting
-            'basic' (default)
-            'conservative'
-            'logcounter8'
-            'logcounter1024'
-        """
 class CountMinSketch(object):
     """
     Data structure used to estimate frequencies of elements in massive data sets with fixed memory footprint.
     To calculate memory footprint:
-        width * depth * cell_size
+        ( width * depth * cell_size ) + HLL size
     Cell size is
-       4B for basic algorithm
+       4B for basic / conservative algorithm
        1B for logcounter8
-       2B for logcounter1024
+       2B for logcounter1024 / logcons1024
+       HLL size is 32 KB
     Example:
-        width 2^26 (67 108 864), depth 4, logcounter1024 (2B) has 2^(26 + 2 + 1) = 536.8 MB
+        width 2^26 (67 108 864), depth 4, logcons1024 (2B) has 2^(26 + 2 + 1) + 32 768 = 536.9 MB
         Can be pickled to disk with this exact size
 
     When choosing parameters, decide on a small depth such as 4 and choose as large width as possible. Increases to width
     are far more effective than increases to depth.
     """
+
     def __init__(self, width=16, depth=8, algorithm=''):
         """
         Initializes the Count-Min Sketch structure with the given parameters
@@ -62,24 +45,28 @@ class CountMinSketch(object):
             'conservative'
             'logcounter8'
             'logcounter1024'
+            'logcons1024'
         """
         self.logshift = 0
         array_type = numpy.uint32
-        self.algorithm = algorithm_basic
         self.inc_method = self.basic_increment
         if algorithm and str(algorithm).lower() == 'conservative':
-            self.algorithm = algorithm_conservative
             self.inc_method = self.conservative_increment
         if algorithm and str(algorithm).lower() == 'logcounter8':
             array_type = numpy.uint8
-            self.algorithm = algorithm_logcounter
             self.logshift = 2
             self.inc_method = self.log_increment
+            self.decode = self.log_decode
         if algorithm and str(algorithm).lower() == 'logcounter1024':
             array_type = numpy.uint16
-            self.algorithm = algorithm_logcounter
             self.logshift = 9
             self.inc_method = self.log_increment
+            self.decode = self.log_decode
+        if algorithm and str(algorithm).lower() == 'logcons1024':
+            array_type = numpy.uint16
+            self.logshift = 9
+            self.inc_method = self.log_increment_conservative
+            self.decode = self.log_decode
 
         self.width = width
         self.depth = depth
@@ -87,12 +74,14 @@ class CountMinSketch(object):
         self.hash_mask = (1 << (self.hash_length - 1)) - 1
         self.logbase = 1 << (self.logshift + 1)
         self.table = numpy.zeros((depth, width), array_type)
+        self.distinct_counter = HyperLogLog(15)
 
     def bucket(self, i, key):
         return self.hash_mask & hash_key(i, key)
 
     def increment(self, key):
         self.inc_method(key)
+        self.distinct_counter.add(str(key))
 
     def basic_increment(self, key):
         for row in range(self.depth):
@@ -106,17 +95,34 @@ class CountMinSketch(object):
                 self.table[row, bucket] += 1
 
     def log_increment(self, key):
+        table = self.table
         for row in range(self.depth):
             bucket = self.bucket(row, key)
-            current = int(self.table[row, bucket])
+            current = int(table[row, bucket])
             if current < 2 * self.logbase:
-                self.table[row, bucket] += 1
+                current += 1
+                self.table[row, bucket] = current
             else:
                 mask = (1 << (current // self.logbase - 1)) - 1
-                r = random.getrandbits(32)
+                r = random.getrandbits(64)
                 if (r & mask) == 0:
                     current += 1
-                    self.table[row, bucket] = current
+                    table[row, bucket] = current
+
+    def log_increment_conservative(self, key):
+        table = self.table
+        buckets = [self.bucket(row, key) for row in range(self.depth)]
+        values = [int(table[row, bucket]) for row, bucket in enumerate(buckets)]
+        current = min(values)
+        if current > 2 * self.logbase:
+            mask = (1 << (current // self.logbase - 1)) - 1
+            r = random.getrandbits(64)
+            if (r & mask) != 0:
+                return
+
+        for row, value in enumerate(values):
+            if value == current:
+                table[row, buckets[row]] = current + 1
 
     def log_decode(self, log_value):
         if (log_value <= 2 * self.logbase):
@@ -136,10 +142,10 @@ class CountMinSketch(object):
         return log_value_base + remainder
 
     def decode(self, value):
-        if self.algorithm == algorithm_logcounter:
-            return self.log_decode(value)
-        else:
-            return value
+        return value
+
+    def __len__(self):
+        return int(self.distinct_counter.cardinality())
 
     def __getitem__(self, key):
         minimum = min(self.table[row, self.bucket(row, key)] for row in range(self.depth))
