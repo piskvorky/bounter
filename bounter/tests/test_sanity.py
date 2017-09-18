82017ba8f55424ca3faaeeb12384638619be3179
ivan <menshikh.iv@gmail.com>
2017-09-18 18:42:21 +0500

    replace tabs to spaces

--- bounter/tests/test_sanity.py
+++ bounter/tests/test_sanity.py
@@ -3,19 +3,19 @@ from bounter import CountMinSketch
 
 
 class TestSanityCheck(unittest.TestCase):
-	def test_sanity(self):
-		cms = CountMinSketch(size_mb=10)
+    def test_sanity(self):
+        cms = CountMinSketch(size_mb=10)
 
-		cms.increment(1)
-		cms.increment(1)
-		cms.increment(1)
-		cms.increment("string")
+        cms.increment(1)
+        cms.increment(1)
+        cms.increment(1)
+        cms.increment("string")
 
-		self.assertEqual(cms[1], 3)
-		self.assertEqual(cms["string"], 1)
-		self.assertEqual(cms.cardinality(), 2)
-		self.assertEqual(cms.sum, 4)
+        self.assertEqual(cms[1], 3)
+        self.assertEqual(cms["string"], 1)
+        self.assertEqual(cms.cardinality(), 2)
+        self.assertEqual(cms.sum, 4)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
