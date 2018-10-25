2957d83c70c383190de6c75618790ed1b90e2d4b
Aneesh <aneeshyjoshi@gmail.com>
2018-10-25 19:17:12 -0400

    add test cases

--- bounter/tests/test_bounter.py
+++ bounter/tests/test_bounter.py
@@ -61,6 +61,12 @@ class BounterInitTest(unittest.TestCase):
         self.assertEqual(counter[u'bar'], 1)
         self.assertEqual(counter.cardinality(), 2)
 
+    def test_contains(self):
+        counter = bounter(size_mb=16)
+        counter.update([u'foo', u'bar', u'foo'])
+        self.assertEqual('foo' in counter, True)
+        self.assertEqual('foobar' in counter, False)
+
     def test_sanity_nocount(self):
         counter = bounter(need_counts=False)
         counter.update([u'foo', u'bar', u'foo'])
@@ -69,3 +75,6 @@ class BounterInitTest(unittest.TestCase):
 
         with self.assertRaises(NotImplementedError):
             print(counter[u'foo'])
+
+if __name__ == '__main__':
+    unittest.main()
